64191df9406eab720f882255fdd3bb1d7492003a
Aamir <82281117+Feugnis@users.noreply.github.com>
2022-08-23 21:06:32 -0400

    Add existing code

---
+++ HelloWorld/src/main/java/com/mycompany/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.helloworld;
+
+public class HelloWorld {
+    public static void
+            main(String[] args) {
+                System.out.println("HelloWorld");
+            }
+}
---
+++ InputDemo/src/main/java/com/mycompany/inputdemo/InputDemo.java
@@ -0,0 +1,23 @@
+package com.mycompany.inputdemo;
+import java.util.Scanner;
+
+public class InputDemo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        System.out.print("Enter a interger: ");
+        long myLong = input.nextLong();
+        System.out.printf("You entered %d.%n%n", myLong);
+        
+        System.out.print("Enter a double: ");
+        double myDouble = input.nextDouble();
+        System.out.printf("You entered %.2f.%n%n", myDouble);
+        
+        System.out.print("Enter a string: ");
+        input.nextLine();
+        String myString = input.nextLine();
+        System.out.printf("You entered %s", myString);
+        System.out.println("You entered " + myString);
+    }
+    
+}
