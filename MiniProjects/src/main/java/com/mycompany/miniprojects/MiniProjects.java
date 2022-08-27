52c90961aa070e9a61ac088feebbd6f60c702dec
Aamir <aamirmazad@gmail.com>
2022-08-27 19:40:14 -0400

    Create MiniProjects.java

---
+++ MiniProjects/src/main/java/com/mycompany/miniprojects/MiniProjects.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.miniprojects;
+
+/**
+ *
+ * @author aamir
+ */
+public class MiniProjects {
+
+    public static void main(String[] args) {
+        for (long i = 0; i < 5; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
